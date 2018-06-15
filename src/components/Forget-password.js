c7953cc55e8ad180dadac6c7764282631119d084
himaniupadhyay <himani@infoud.co.in>
2018-06-15 15:43:04 +0530

    forgetpwdwith changes

--- src/components/Forget-password.js
+++ src/components/Forget-password.js
@@ -10,9 +10,9 @@ class Forgetpassword extends JSXComponent {
         return (
             <form>
                 <center>
-
-                    <label for="email"> Enter Email: <input type="text" placeholder="EmailID" name="email" /> </label><br />
-                    <label for="username"> Enter Username: <input type="text" placeholder="Username"  name="username" /> </label><br />
+        <h3>Forget Password..??</h3><br />
+                    <label for="email"> Enter Email: <input type="text" placeholder="EmailID" name="email" /> </label><br /><br />
+                    <label for="username"> Enter Username: <input type="text" placeholder="Username"  name="username" /> </label><br /><br />
                     <button type="submit" id="space" class="btn btn-primary" onClick={this.submitMsg.bind(this)}>Submit</button>
                     <button type="submit" class="btn btn-primary">SignIn</button>
                 </center>
