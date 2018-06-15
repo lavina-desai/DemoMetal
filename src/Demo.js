dc0859ca41e5cc6d25c234b4a9a7f0bcbb2904f5
Lavina Desai <lavina.desai@infoud.co.in>
2018-06-15 16:24:42 +0530

    signin and resetpassword component created

--- src/Demo.js
+++ src/Demo.js
@@ -4,21 +4,30 @@ import JSXComponent from 'metal-jsx';
 
 import './demo.scss';
 import SearchBarComponent from './components/search-bar';
+import Signin from './components/Signin';
+import { Resetpassword } from './components/Resetpassword';
 
 class Demo extends JSXComponent {
 	render() {
 		return <div class="demo">
 			Hello World
 			<SearchBarComponent
-			events={{
-				onSearch: this.handleOnSearch.bind(this)
-			}}></SearchBarComponent>
-			</div>;
+				events={{
+					onSearch: this.handleOnSearch.bind(this)
+				}}></SearchBarComponent>
+
+			<br /><br />
+			<Signin></Signin>
+			<br /><br /><br />
+			<Resetpassword></Resetpassword>
+
+		</div>;
 	}
 
 	handleOnSearch(event) {
-        console.log(event.text);
-    }
+		console.log(event.text);
+	}
+
 }
 
 export { Demo };
